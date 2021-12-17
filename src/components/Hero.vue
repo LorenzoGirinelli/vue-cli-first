5da1fab909f4567d006984568496f280d5829385
LorenzoGirinelli <girinellilorenzo22@hotmail.com>
2021-12-17 17:44:31 +0100

    bonus

---
+++ src/components/Hero.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <h2>Hero</h2>
+        <img src="../assets/img/jumbo.png" alt="">
+    </div>
+</template>
+
+<script>
+    export default {
+  name: "Hero",
+};
+</script>
